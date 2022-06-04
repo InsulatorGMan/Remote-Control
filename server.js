9b5aaf45f8383788fa7e05404495a39ce76572ed
Grant Stahr <76236444+InsulatorGMan@users.noreply.github.com>
2022-06-04 17:24:03 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+
+//**Settings **//
+let timeout = 5;
+let allow_deleting = true; //Default [true]
+let allow_notifications = true; //Default = [true]
+let allow_popups = true; //Defautl = [true]
+app.listen(PORT, () => {
+    console.log
+});
